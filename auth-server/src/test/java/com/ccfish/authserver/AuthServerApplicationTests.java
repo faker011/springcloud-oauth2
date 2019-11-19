215b277027802c784d22055d9b4ea66270602c67
liyihan <liyihan@genius.com>
2019-11-19 14:12:25 +0800

    init oauth2 test

---
+++ auth-server/src/test/java/com/ccfish/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccfish.authserver;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ service-hi/src/test/java/com/ccfish/servicehi/ServiceHiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccfish.servicehi;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceHiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
